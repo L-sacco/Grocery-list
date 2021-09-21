8afd57fe34c548099a964c10c62fb0477514915f
Luigi Sacco <luigi.sa94@gmail.com>
2021-09-21 17:01:27 +0200

    style + visual list + visual remove

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 var form = document.querySelector(".grocery-form");
 var input = document.getElementById("grocery-input");
-var submitBtn = document.querySelector(".submit-btn");
-var list = document.querySelector(".list");
-var clearBtn = document.querySelector(".clear-tbn");
-var itemID = new Date().getTime().toString();
+var submitBtn = document.querySelector(".submit-btn"); // unused
+var list = document.querySelector(".list-container");
+var clearBtn = document.querySelector(".clear-tbn"); // unused
+var itemCounter = 0;
 form.addEventListener("submit", AddItem);
 function AddItem(ev) {
     ev.preventDefault();
-    console.log(itemID);
-    console.log(input.value);
     if (input.value) {
         CreateElement(input.value);
     }
+    console.log(++itemCounter);
+    if (itemCounter > 0) {
+        list.classList.add('show');
+    }
+    BackToDefault();
 }
 function CreateElement(value) {
-    var elem = document.createElement('div');
+    var itemID = new Date().getTime().toString();
+    var element = document.createElement('div');
+    element.classList.add('list');
     var attr = document.createAttribute('data-id');
     attr.value = itemID;
-    elem.setAttributeNode(attr);
-    elem.innerHTML = '<div class="list">' + input.value + '</div><button class="clear-btn">Clear list</button>';
-    list.appendChild(elem);
+    element.setAttributeNode(attr);
+    element.innerHTML =
+        "<p class='item'>" + input.value +
+            "</p><button class='single-remove-btn' onclick='RemoveSingleItem(" + itemID +
+            ")'><img src='img/bin.png' /></button>";
+    list.appendChild(element);
+    AddToLocalStorage(itemID, input.value);
+}
+function AddToLocalStorage(id, value) {
+    console.log("added");
+}
+function BackToDefault() {
+    input.value = "";
+    input.focus();
+}
+function RemoveEverything() {
+    var anchor = document.querySelector('div[class="list"]');
+    anchor.remove();
+}
+function RemoveSingleItem(id) {
+    var anchor = document.querySelector('div[data-id=\'' + id + '\']');
+    anchor.remove();
 }
--- app.ts
+++ app.ts
@@ -1,26 +1,60 @@
-const form = document.querySelector(".grocery-form") as HTMLInputElement;
+const form = document.querySelector(".grocery-form") as HTMLFormElement;
 const input = document.getElementById("grocery-input") as HTMLInputElement;
-const submitBtn = document.querySelector(".submit-btn") as HTMLInputElement;
-const list = document.querySelector(".list") as HTMLInputElement;
-const clearBtn = document.querySelector(".clear-tbn") as HTMLInputElement;
+const submitBtn = document.querySelector(".submit-btn") as HTMLButtonElement; // unused
+const list = document.querySelector(".list-container") as HTMLDivElement;
+const clearBtn = document.querySelector(".clear-tbn") as HTMLButtonElement; // unused
 
-const itemID = new Date().getTime().toString();
+var itemCounter = 0;
+
 form.addEventListener("submit", AddItem);
 
 function AddItem(ev) {
     ev.preventDefault();
-    console.log(itemID);
-    console.log(input.value);
     if(input.value) {
         CreateElement(input.value);
     }
+    console.log(++itemCounter);
+
+    if(itemCounter > 0) { list.classList.add('show'); }
+    BackToDefault();
 }
 
 function CreateElement(value) {
-    let elem = document.createElement('div') as HTMLDivElement;
+    var itemID = new Date().getTime().toString();
+    let element = document.createElement('div') as HTMLDivElement;
+    element.classList.add('list');
     const attr = document.createAttribute('data-id');
     attr.value = itemID;
-    elem.setAttributeNode(attr);
-    elem.innerHTML = '<div class="list">' + input.value + '</div><button class="clear-btn">Clear list</button>';
-    list.appendChild(elem);
+    element.setAttributeNode(attr);
+    element.innerHTML = 
+        "<p class='item'>" + input.value + 
+        "</p><button class='single-remove-btn' onclick='RemoveSingleItem(" + itemID + 
+        ")'><img src='img/bin.png' /></button>";
+    list.appendChild(element);
+
+    AddToLocalStorage(itemID, input.value);
+}
+
+function AddToLocalStorage(id, value) {
+    console.log("added")
+}
+
+function BackToDefault() {
+    input.value = "";
+    input.focus();
+}
+
+function RemoveEverything() {
+    //
+    //
+    //
+
+    // todo: undo button
+}
+
+function RemoveSingleItem(id) {
+    let anchor = document.querySelector('div[data-id=\'' + id + '\']');
+    anchor.remove();
+
+    // todo: undo button
 }
